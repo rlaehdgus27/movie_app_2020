a85f953395e270fcc5b20d6abeda5833aa84e41e
DEV_KDH <rlaehdgus27@gmail.com>
2020-10-19 18:57:45 +0900

    1.1 Creating your first React App

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+function Food ({name}){
+return <h3>I like {name}</h3>;
+}
+
+const foodLike = [{name:"mouse"},{name:"notebook"},{name:"phone"}]
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {foodLike.map(dish => <Food name={dish.name} /> )}
     </div>
-  );
+    );
 }
 
 export default App;
