2f674f3ff401f279a8df098df5b458b32e967917
김동현 <dong@gimdonghyeon-ui-MacBookPro.local>
2020-10-20 22:57:43 +0900

    Updates

---
+++ precache-manifest.58785a69c76b42b30d0854d025672ed7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f641f4ae49ebac13fe9be360f38e7d00",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "0d3befefdf8630071754",
+    "url": "/movie_app_2020/static/css/main.83b72409.chunk.css"
+  },
+  {
+    "revision": "f575d44c1b0e9225b766",
+    "url": "/movie_app_2020/static/js/2.122f7658.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/movie_app_2020/static/js/2.122f7658.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d3befefdf8630071754",
+    "url": "/movie_app_2020/static/js/main.ff4a0dc3.chunk.js"
+  },
+  {
+    "revision": "6636fcdddd453d60abc3",
+    "url": "/movie_app_2020/static/js/runtime-main.9a083715.js"
+  }
+]);
