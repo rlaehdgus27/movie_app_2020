8f3be9c215d7fc757e039651a68f4d492c05d4ee
김동현 <dong@gimdonghyeon-ui-MacBookPro.local>
2020-10-20 23:04:30 +0900

    Update movie_app_2020

--- src/App.js
+++ src/App.js
@@ -1,17 +1,55 @@
-import React from 'react';
+import React from "react";
+import axios from "axios";
+import Movie from "./Movie";
+import "./css/App.css";
 
-function Food ({name}){
-return <h3>I like {name}</h3>;
-}
+class App extends React.Component {
+  state = {
+    isLoading: true,
+    movie: [],
+  };
 
-const foodLike = [{name:"mouse"},{name:"notebook"},{name:"phone"}]
+  // async는 await는 짝꿍이다 같이 써야하는 것
 
-function App() {
-  return (
-    <div>
-      {foodLike.map(dish => <Food name={dish.name} /> )}
-    </div>
+  getMovies = async () => {
+    const {
+      data: {
+        data: { movies },
+      },
+    } = await axios.get(
+      "https://yts-proxy.now.sh/list_movies.json?sort_by=rating"
     );
+    this.setState({ movies, isLoading: false });
+  };
+  componentDidMount() {
+    this.getMovies();
+  }
+  render() {
+    const { isLoading, movies } = this.state;
+    return (
+      <section className="container">
+        {isLoading ? (
+          <div className="loader">
+            <span className="loader__text">Loading...</span>
+          </div>
+        ) : (
+          <div className="movies">
+            {movies.map((movie) => (
+              <Movie
+                key={movie.id}
+                id={movie.id}
+                year={movie.year}
+                title={movie.title}
+                summary={movie.summary}
+                poster={movie.medium_cover_image}
+                genres={movie.genres}
+              />
+            ))}
+          </div>
+        )}
+      </section>
+    );
+  }
 }
 
 export default App;
